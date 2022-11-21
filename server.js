daaf7aae681db59aa29d6f874478a0371c4f79bc
seungryeol <opop0421@naver.com>
2022-11-22 01:38:53 +0900

    56

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const app = express();
 app.get('/', async (req,res) => {
     
     const CLIENT_BASE_URL = "ubuntu_redis-server_1";
-
+/*
     const client = redis.createClient({
            url: `redis://${CLIENT_BASE_URL}:6379`,
         // port:6379
         });
     await client.connect();
-    await client.set("1", "이승렬");
+    await client.set("1", "이승렬");*/
    res.send("ok");
 
 });
